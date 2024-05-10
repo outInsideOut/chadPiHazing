9e99fe2b014d37caf6dfd9ef40d8f30f9f992948
Fionn O'Connor <Fionn O'Connor>
2024-05-10 22:59:04 +0000

    rust-analyzer

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -11,7 +11,7 @@ return {
     opts = function(_, opts)
       -- add more things to the ensure_installed table protecting against community packs modifying it
       opts.ensure_installed = require("astrocore").list_insert_unique(opts.ensure_installed, {
-        "lua_ls",
+        "lua_ls", "rust-analyzer"
         -- add more arguments for adding more language servers
       })
     end,
